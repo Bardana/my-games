26202e1545757f7be0fac34395e8220242848b21
Bardana <66261690+Bardana@users.noreply.github.com>
2021-04-28 15:14:10 +0200

    Create meals.js

---
+++ src/components/meals.js
@@ -0,0 +1,32 @@
+[
+    {
+        "id": 0,
+        "name": "Dinner",
+        "image": "https://cdn.pixabay.com/photo/2015/03/26/10/28/restaurant-691397_1280.jpg",
+        "category": "mains",
+        "label": "Hot peppers tomatoes and greens",
+        "price": "4.99",
+        "featured": true,
+        "description": "During the times of Ancient Rome, a dinner party was referred to as a convivia, and was a significant event for Roman emperors and senators to congregate and discuss their relations. The Romans often ate and were also very fond of a fish sauce called liquamen (also known as Garum) during said parties."
+    },
+    {
+       "id": 0,
+        "name": "Brunch",
+        "image": "https://cdn.pixabay.com/photo/2017/01/26/02/06/platter-2009590_1280.jpg",
+        "category": "Breakfast and lunch",
+        "label": "Hot peppers tomatoes and greens",
+        "price": "19.99",
+        "featured": true,
+        "description": "Brunch is a combination of breakfast and lunch, and regularly has some form of alcoholic drink (most usually champagne or a cocktail) served with it. It is usually served anytime before 3 o'clock in the afternoon. The word is a portmanteau of breakfast and lunch. Brunch originated in England in the late 19th century and became popular in the United States in the 1930s."
+    },
+     {
+        "id": 3,
+        "name": "Children's Menu",
+        "image": "https://cdn.pixabay.com/photo/2016/09/15/19/24/salad-1672505_1280.jpg",
+        "category": "Kids",
+        "label": "Hot peppers tomatoes and greens",
+        "price": "CM",
+        "featured": true,
+        "description": "Youngsters have become more sophisticated in their palates and seek items from the regular menu but in smaller servings. The first kids' meal, Funmeal, emerged at Burger Chef in 1973 and succeeded."
+        }
+ ]
